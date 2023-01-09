62b6a20acbcfee72c6574a51a3a3536edcd33e05
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-01-09 08:34:33 +0600

    splash_screen

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: library_private_types_in_public_api, prefer_const_literals_to_create_immutables, unused_field, prefer_const_constructors, sized_box_for_whitespace
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_covid_19_app/View/home_page.dart';
+import 'dart:math' as math;
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 3), vsync: this)
+        ..repeat();
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => HomePageScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: AnimatedBuilder(
+                  animation: _controller,
+                  child: Container(
+                    width: 200.w,
+                    height: 200.h,
+                    child: Center(child: Image.asset("image/virus.png")),
+                  ),
+                  builder: (BuildContext context, child) {
+                    return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                      child: child,
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Covid-19\nTracker App',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.green),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
